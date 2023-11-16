009bd71a5af4b28b6106aab61b5fad76731226c5
cristinagiacobbe <crigiak@gmail.com>
2023-11-16 22:47:52 +0100

    Define tools

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Il programma dovrà chiedere all'utente di inserire:
+- numero chilometri da percorrere
+- età del passeggero
+
+Deve poi calcolare il prezzo del biglietto alle seguenti condizioni:
+- il costo del biglietto è 0,21€/Km
+- il biglietto è scontato del 20% per passeggeri <18
+- il biglietto è scontato del 40% per passeggeri >65
+
+Stampare in pagina il prezzo finale del biglietto espresso con 2 decimali */
+
+//TOOLS
+//const: number_km, passenger_age, ticket_price
+//prompt to ask user km and age
+//2 conditions for discount
+//calculate ticket_price (conditional result)
+//document in html
+
